fe67004d3b890da20499255b5ea8ebffd1310d6a
Matthew Volesky <mvoleskym@gmail.com>
2024-12-09 19:15:46 +0000

    Completed course

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,20 @@
+variable "cidr_block" {
+  type        = string
+  description = "The CIDR block assigned to the VPC"
+}
+
+variable "enable_igw" {
+  type        = bool
+  description = "Toggle creation of the IGW"
+  default     = true
+}
+
+variable "env" {
+  type        = string
+  description = "The AWS environment"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region"
+}
---
+++ modules/vpc_subnets/variables.tf
@@ -0,0 +1,32 @@
+variable "env" {
+  type        = string
+  description = "The AWS environment"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region"
+}
+
+variable "subnets" {
+  type = list(object({
+    name              = string
+    cidr_block        = string
+    is_public         = bool
+    availability_zone = string
+  }))
+}
+
+variable "public_route_table_id" {
+  type = string
+}
+
+
+variable "private_route_table_id" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
